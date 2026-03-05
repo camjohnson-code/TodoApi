5ac4cb56b5301cda0f6f1f9b795bee787a421041
Cam Johnson <cameron.n.johnson15@gmail.com>
2026-03-05 13:25:09 -0700

    Add DTO

---
+++ DTOs/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.DTOs;
+
+public class RegisterRequest
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
